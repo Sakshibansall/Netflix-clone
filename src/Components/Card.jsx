b8b95c03fb48acc1236681d5fa996fcecd591b52
Sakshibansall <156604991+Sakshibansall@users.noreply.github.com>
2025-09-12 23:55:26 +0530

    changes

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,14 +4,14 @@
 function Card({ heading,text,image}) {
   return (
     <div className=' '>
-      <div className=' lg:w-73 w-full h-full  lg:h-95 rounded-2xl pt-7  px-4 flex  flex-col  justify-between' style={{
+      <div className=' lg:w-70   w-full h-full  lg:h-90 rounded-2xl pt-7  px-4 flex  flex-col  justify-between' style={{
         backgroundImage: "linear-gradient(149deg, #192247 0%, #210e17 99.08%)"
       }}>
 
 
         <div >
           <h2 className='lg:text-3xl text-xl text-white font-bold pb-4'>{heading}</h2>
-          <p className='text-zinc-300   font-semibold lg:text-xl text-m leading-6 tracking-wide '>{text}</p>
+          <p className='text-zinc-300  font-semibold lg:text-xl text-m leading-6 tracking-wide '>{text}</p>
         </div>
         
        {image &&( <div className='flex justify-end '>
